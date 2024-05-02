725b34243e9719d6bc749119bc4af96cfd73146e
Muhammad Anas <manasansari1438@gmail.com>
2024-05-02 00:43:54 -0700

    changes

--- client/src/layout/NavBar.js
+++ client/src/layout/NavBar.js
@@ -89,12 +89,12 @@ const NavBar = () => {
             {Navlinks.map((link, index) => {
               return (
                 <li key={index}>
-                  <Link
-                    to={`${link.path}`}
+                  <a
+                    href={`${link.path}`}
                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     {link.name}
-                  </Link>
+                  </a>
                 </li>
               );
             })}
