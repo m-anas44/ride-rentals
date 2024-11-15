807caa84d602ef5cb2b387ab08bf68aacb6d6f92
Muhammad Anas <manasansari1438@gmail.com>
2024-11-14 20:42:14 -0800

    cors added

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: `${process.env.CORS_ORIGIN}`,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
